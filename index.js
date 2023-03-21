267e5cec78ef0c72476eaaf131886e75f8ff9cf3
Arturo Ceceña <arturocecena@hotmail.com>
2023-03-21 09:47:41 -0600

    Ejercicio descuento

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-/*Escribe una función llamada "calcularDescuento" que reciba dos parámetros: el precio de un producto y un descuento en porcentaje. La función debe calcular el precio 
-con el descuento aplicado y retornar este valor. Sin embargo, si el descuento es mayor al 50%, la función debe retornar un mensaje que indique que el descuento no es válido. 
-Luego, utiliza esta función para calcular el precio con un descuento del 20% y del 60% en un producto que cuesta $100 y despliega el resultado en un alert()*/
+/*Escribe una función llamada "calcularDescuento" que reciba dos parámetros: el precio de un producto y un descuento 
+en porcentaje. La función debe calcular el precio 
+con el descuento aplicado y retornar este valor. 
+Sin embargo, si el descuento es mayor al 50%, la función debe retornar un mensaje que indique 
+que el descuento no es válido. 
+Luego, utiliza esta función para calcular el precio con un descuento del 20% y del 60% en un producto
+ que cuesta $100 y despliega el resultado en un alert()*/
 
 /*El precio con un descuento del 20% es: 80
 El precio con un descuento del 60% es: Descuento no válido*/
-
 
 const precio = 100;
 
@@ -12,37 +15,21 @@ var porc20 = 20;
 
 var porc80 = 80;
 
-function calcularDescuento(precio_producto, descuento_porcentaje){
+function calcularDescuento(precio_producto, descuento_porcentaje) {
+  precio_producto = window.prompt(`Precio del producto`);
 
-    
-    precio_producto = window.prompt(`Precio del producto`)
-    
-    descuento_porcentaje = window.prompt(`Descuento que se quiere aplicar`)
+  descuento_porcentaje = window.prompt(`Descuento que se quiere aplicar`);
 
-    porc20 = (20 / 100) * precio 
-    porc20 = precio - porc20
-
-    porc80 = (80 / 100) * precio 
-    porc80 = precio - porc80
-
-
-
-    if(descuento_porcentaje >= 50){
-        return window.alert(`El precio con un descuento del ${descuento_porcentaje}% es: Descuento no válido.`)
-
-    }
-    else{
-        var total = (descuento_porcentaje / 100) * precio_producto
-        var total = precio_producto - total
-        return window.alert(`El total de su producto con el descuento aplicado es de: ${total}`)
-
-    }
-
-
-
-    
-    
-
-
+  if (descuento_porcentaje >= 50) {
+    return window.alert(
+      `El precio con un descuento del ${descuento_porcentaje}% es: Descuento no válido.`
+    );
+  } else {
+    var total = (descuento_porcentaje / 100) * precio_producto;
+    var total = precio_producto - total;
+    return window.alert(
+      `El total de su producto con el descuento aplicado es de: ${total}`
+    );
+  }
 
 }
